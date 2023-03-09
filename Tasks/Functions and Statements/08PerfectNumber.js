723d39c7c6aa167118214e73fa7249128229ea9d
Natali Filipova <56434467+NataliFilipova@users.noreply.github.com>
2023-03-09 21:44:44 +0200

    homework task

--- Tasks/Functions and Statements/08PerfectNumber.js
+++ Tasks/Functions and Statements/08PerfectNumber.js
@@ -1,3 +1,8 @@
 function perfectNumber(number){
-    
+    let divisors = [];
+    for(let i = 1; i< number; i++){
+        if(number % i === 0){
+            divisors.push(index);
+        }
+    }
 }
